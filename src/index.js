dc047a669bc0cdee6c9c85520b09d2ad85bc4e5f
huba.gaspar <gaspar.huba91@gmail.com>
2021-05-28 20:34:54 +0200

    index.js typescript support, added exported type PinataClient

--- src/index.js
+++ src/index.js
@@ -12,113 +12,153 @@ import pinList from './commands/data/pinList/pinList';
 import userPinnedDataTotal from './commands/data/userPinnedDataTotal';
 
 /**
+ * Pin by hash
+ * @callback pinByHash
+ * @param {*} hashToPin
+ * @param {*} options
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * Hash meta data
+ * @callback hashMetadata
+ * @param {*} ipfsPinHash
+ * @param {*} metadata
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * Hash pin policy
+ * @callback hashPinPolicy
+ * @param {*} ipfsPinHash
+ * @param {*} newPinPolicy
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * Pin file to IPFS
+ * @callback pinFileToIPFS
+ * @param {*} readableStream
+ * @param {*} options
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * Pin from FS
+ * @callback pinFromFS
+ * @param {*} sourcePath
+ * @param {*} options
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * Pin JSON to IPFS
+ * @callback pinJSONToIPFS
+ * @param {*} body
+ * @param {*} options
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * Pin Jobs
+ * @callback pinJobs
+ * @param {*} filters
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * Unpin
+ * @callback unpin
+ * @param {*} hashToUnpin
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * User Pin Policy
+ * @callback userPinPolicy
+ * @param {*} newPinPolicy
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * Test Authentication
+ * @callback testAuthentication
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * Pin List
+ * @callback pinList
+ * @param {*} filters
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * User Pinned Data Total
+ * @callback userPinnedDataTotal
+ * @returns {Promise | Promise<unknown>}
+ */
+
+/**
+ * @typedef PinataClient
+ * @property {pinByHash} pinByHash
+ * @property {hashMetadata} hashMetadata
+ * @property {hashPinPolicy} hashPinPolicy
+ * @property {pinFileToIPFS} pinFileToIPFS
+ * @property {pinFromFS} pinFromFS
+ * @property {pinJSONToIPFS} pinJSONToIPFS
+ * @property {pinJobs} pinJobs
+ * @property {unpin} unpin
+ * @property {userPinPolicy} userPinPolicy
+ * @property {testAuthentication} testAuthentication
+ * @property {pinList} pinList
+ * @property {userPinnedDataTotal} userPinnedDataTotal
+ */
+
+/**
  * Pinata Client
  *
  * @param {string} pinataApiKey
  * @param {string} pinataSecretApiKey
- * @returns {{}}
+ * @returns {PinataClient}
  */
 export default function pinataClient(pinataApiKey, pinataSecretApiKey) {
     let client = {};
 
     //  setting up the actual calls you can make using this package
-    /**
-     * Pin by hash
-     * @param {*} hashToPin
-     * @param {*} options
-     * @returns {Promise | Promise<unknown>}
-     */
     client.pinByHash = function (hashToPin, options) {
         return pinByHash(pinataApiKey, pinataSecretApiKey, hashToPin, options);
     };
-    /**
-     * Hash meta data
-     * @param {*} ipfsPinHash
-     * @param {*} metadata
-     * @returns {Promise | Promise<unknown>}
-     */
     client.hashMetadata = function (ipfsPinHash, metadata) {
         return hashMetadata(pinataApiKey, pinataSecretApiKey, ipfsPinHash, metadata);
     };
-    /**
-     * Hash pin policy
-     * @param {*} ipfsPinHash
-     * @param {*} newPinPolicy
-     * @returns {Promise | Promise<unknown>}
-     */
     client.hashPinPolicy = function (ipfsPinHash, newPinPolicy) {
         return hashPinPolicy(pinataApiKey, pinataSecretApiKey, ipfsPinHash, newPinPolicy);
     };
-    /**
-     * Pin file to IPFS
-     * @param {*} readableStream
-     * @param {*} options
-     * @returns {Promise | Promise<unknown>}
-     */
     client.pinFileToIPFS = function (readableStream, options) {
         return pinFileToIPFS(pinataApiKey, pinataSecretApiKey, readableStream, options);
     };
-    /**
-     * Pin from FS
-     * @param {*} sourcePath
-     * @param {*} options
-     * @returns {Promise | Promise<unknown>}
-     */
     client.pinFromFS = function (sourcePath, options) {
         return pinFromFS(pinataApiKey, pinataSecretApiKey, sourcePath, options);
     };
-    /**
-     * Pin JSON to IPFS
-     * @param {*} body
-     * @param {*} options
-     * @returns {Promise | Promise<unknown>}
-     */
     client.pinJSONToIPFS = function (body, options) {
         return pinJSONToIPFS(pinataApiKey, pinataSecretApiKey, body, options);
     };
-    /**
-     * Pin Jobs
-     * @param {*} filters
-     * @returns {Promise | Promise<unknown>}
-     */
     client.pinJobs = function (filters) {
         return pinJobs(pinataApiKey, pinataSecretApiKey, filters);
     };
-    /**
-     * Unpin
-     * @param {*} hashToUnpin
-     * @returns {Promise | Promise<unknown>}
-     */
     client.unpin = function (hashToUnpin) {
         return unpin(pinataApiKey, pinataSecretApiKey, hashToUnpin);
     };
-    /**
-     * User Pin Policy
-     * @param {*} newPinPolicy
-     * @returns {Promise | Promise<unknown>}
-     */
     client.userPinPolicy = function (newPinPolicy) {
         return userPinPolicy(pinataApiKey, pinataSecretApiKey, newPinPolicy);
     };
-    /**
-     * Test Authentication
-     * @returns {Promise | Promise<unknown>}
-     */
     client.testAuthentication = function () {
         return testAuthentication(pinataApiKey, pinataSecretApiKey);
     };
-    /**
-     * Pin List
-     * @param {*} filters
-     * @returns {Promise | Promise<unknown>}
-     */
     client.pinList = function (filters) {
         return pinList(pinataApiKey, pinataSecretApiKey, filters);
     };
-    /**
-     * User Pinned Data Total
-     * @returns {Promise | Promise<unknown>}
-     */
     client.userPinnedDataTotal = function () {
         return userPinnedDataTotal(pinataApiKey, pinataSecretApiKey);
     };
