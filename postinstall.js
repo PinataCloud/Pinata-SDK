db1a06fd569bc60cb573b799c170a201074ff7c2
Steve <stevedsimkins@gmail.com>
2024-08-05 22:28:24 -0400

    added changes to Readme and postinstall script

---
+++ postinstall.js
@@ -0,0 +1,11 @@
+console.log("\n\x1b[33m=================================\x1b[0m");
+console.log("\x1b[1m\x1b[36mATTENTION!\x1b[0m");
+console.log(
+	"\x1b[36mThis SDK has been archived, please install the new SDK:\x1b[0m",
+);
+console.log("\x1b[36m \x1b[0m");
+console.log("\x1b[36mnpm i pinata\x1b[0m");
+console.log("\x1b[36m \x1b[0m");
+console.log("\x1b[36mPlease visit our docs for more information:\x1b[0m");
+console.log("\x1b[36mhttps://docs.pinata.cloud/ipfs-sdk\x1b[0m");
+console.log("\x1b[33m=================================\x1b[0m\n");
